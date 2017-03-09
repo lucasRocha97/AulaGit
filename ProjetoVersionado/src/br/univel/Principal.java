d95b5b87e6cc1317eb6081d9dacf8325743ee8b7
lucasRocha97 <lucas_rochaubta@hotmail.com>
2017-03-09 01:21:38 -0300

    Alteração após a criação da tag.

--- ProjetoVersionado/src/br/univel/Principal.java
+++ ProjetoVersionado/src/br/univel/Principal.java
@@ -2,7 +2,8 @@ package br.univel;
 
 public class Principal {
 	public static void main(String[] args) {
-		String msg = "Hello World!!!";
+		String world = "World";
+		String msg = "Hello " + world + "!!!";
 		System.out.println(msg);
 	}
 }
