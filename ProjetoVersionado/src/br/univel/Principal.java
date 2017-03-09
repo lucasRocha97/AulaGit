0dcd6279f6d5033fb190b9c52126491e120cafb9
lucasRocha97 <lucas_rochaubta@hotmail.com>
2017-03-09 01:48:08 -0300

    Ultima alteração(só adicioanei "LUCAS").

--- ProjetoVersionado/src/br/univel/Principal.java
+++ ProjetoVersionado/src/br/univel/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		String hello = "Hello ";
 		String world = "World";
-		String msg = hello  + world + "!!!";
+		String msg = hello  + world + "!!!" + "LUCAS";
 		System.out.println(msg);
 	}
 }
