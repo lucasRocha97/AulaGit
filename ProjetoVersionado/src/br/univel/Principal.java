391458f1fdba8d7bd789aed5b02b06fbd081b384
lucasRocha97 <lucas_rochaubta@hotmail.com>
2017-03-09 01:28:09 -0300

    Alteração solicitada pelo cliente.

--- ProjetoVersionado/src/br/univel/Principal.java
+++ ProjetoVersionado/src/br/univel/Principal.java
@@ -3,6 +3,8 @@ package br.univel;
 public class Principal {
 	public static void main(String[] args) {
 		String msg = "Hello World!!!";
+		System.out.println("--------------------");
 		System.out.println(msg);
+		System.out.println("--------------------");
 	}
 }
